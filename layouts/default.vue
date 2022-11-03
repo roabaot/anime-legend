a779e7ff31590cbcf30d01b85c1b9c351bab1e3a
ahmed <a-ahmed987@outlook.com>
2022-11-03 16:36:39 +0300

    add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <CoreDrawer v-if="$vuetify.breakpoint.smAndDown" :drawer="drawer" @drawer="drawer = $event" />
+    <CoreHeader :drawer="drawer" @drawer="drawer = $event" />
+    <!-- <SideBar /> -->
+    <v-main id="main">
+      <nuxt />
+      <CoreFooter />
+    </v-main>
+    <!-- <Snackbar /> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Default',
+  data () {
+    return {
+      title: 'Anime Legend',
+      drawer: false
+    }
+  },
+  mounted () {
+    this.$meta().refresh()
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/index.scss';
+
+.v-main__wrap {
+  // height: 100vh;
+  // overflow-y: auto !important;
+}
+</style>
